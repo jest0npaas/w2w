bb89776bc99ee0ee0c02c4d3b5c7785124843f11
Aga Mecarte <aga.mecarte@outlook.com>
2025-05-27 19:42:32 +0800

    add results to logged in user

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,18 +7,20 @@ class PagesController < ApplicationController
 
   def results
     # real API call
-    @movies = MovieApi.get_movies_by_title(params[:query])["Search"]
+    # @movies = MovieApi.get_movies_by_title(params[:query])["Search"]
 
 
     # mock API data call
-    # file_path = Rails.root.join("lib", "assets", "search.json")
-    # @movies = JSON.parse(File.read(file_path))["Search"]
+    file_path = Rails.root.join("lib", "assets", "search.json")
+    @movies = JSON.parse(File.read(file_path))["Search"]
 
-    @movies.each do |movie|
-      Movie.find_or_create_by(api_movie_id: movie["imdbID"]) do |m|
-        m.title = movie["Title"]
-        m.poster = movie["Poster"]
-        m.user_id = 3 # to be changed to current_user
+    if user_signed_in?
+      @movies.each do |movie|
+        Movie.find_or_create_by(api_movie_id: movie["imdbID"]) do |m|
+          m.title = movie["Title"]
+          m.poster = movie["Poster"]
+          m.user_id = current_user.id
+        end
       end
     end
   end
